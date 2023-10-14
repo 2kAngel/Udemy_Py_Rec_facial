7dde2199f6e76349f17e31fefa637e5c4d81f6ea
ANGEL CAMARA RAMOS <103178330+2kAngel@users.noreply.github.com>
2023-10-14 13:01:53 +0200

    5/6

---
+++ prueba_0.py
@@ -0,0 +1,50 @@
+print("hola mundo")
+print("2")
+
+numero = 2.99
+print(numero)
+print(type(numero))
+
+string = "Cadena"
+print(string)
+print(type(string))
+
+booleano = True
+print(booleano)
+print(type(booleano))
+
+num2 = 6
+suma = numero+num2
+print("Suma -> ",suma)
+
+#comentario
+
+'''Comentario
+grupal
+'''
+
+#operadores
+res1 = numero+num2 
+res2 = numero-num2 
+res3 = numero*num2 
+res4 = numero/num2 
+res5 = numero%num2 
+print(res1,"------",res2,"------",res3,"------",res4,"------",res5)
+
+#and or not
+r = (res1>res2 and res2>res1)
+print(r)
+
+#-------salida de datos
+'''
+app = "flutter"
+proyecto = "ComFlu"
+print(f"se hara en" {app} y se llamara {proyecto})
+'''
+
+
+# enrada de datos
+
+cad = int(input("¿version?: "))
+print(f"tu proy se llama {cad+4}")
+
